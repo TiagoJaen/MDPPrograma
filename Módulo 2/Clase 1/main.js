d312b12792d97d1e04f628c9947ac0ce3423229e
TiagoJaen <tiagojaen123@gmail.com>
2023-08-11 14:08:10 -0300

    Mód. 2

---
+++ Módulo 2/Clase 1/main.js
@@ -0,0 +1,9 @@
+console.log("Hola mundo");
+// 1)
+console.log("Tiago Jaen");
+// 2)
+console.log("9+2=" + (9+2))
+// 3)
+console.log("Tiago Jaen, " + "18 años.")
+// 4)
+console.log("2>1 = " + (2>1))
---
+++ Módulo 2/Clase 2/main.js
@@ -0,0 +1,4 @@
+var nombre = prompt("¿Cual es tu nombre?");
+var edad = prompt("¿Cual es tu edad?");
+
+console.log("Nombre: " + nombre + ". Edad: " + edad + ".")
